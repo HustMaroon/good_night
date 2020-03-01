b50c48b0f59c01e88ed708d15d528d5b823866ce
MLHustMaroon <doxuan.thanh@monstar-lab.com>
2020-03-02 03:47:42 +0900

    Model spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:clock_ins).dependent(:destroy) }
+  it { should have_many(:active_relationships).dependent(:destroy) }
+  it { should have_many(:following) }
+  it { should have_many(:passive_relationships).dependent(:destroy) }
+  it { should have_many(:followers) }
 end
