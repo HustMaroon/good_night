7dedb19800fad346a95fb8fe885ae0a6245c372f
MLHustMaroon <doxuan.thanh@monstar-lab.com>
2020-03-03 00:18:32 +0900

    Request spec for relationship api

---
+++ spec/requests/relationship_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+describe 'relationship', type: :request do
+  let!(:follower) { create :user }
+  let!(:followed) { create :user }
+
+  # to follow an user
+  # required params: follower_id, followed_id
+  describe 'POST /api/follow' do
+    let(:execute) { post '/api/follow', params: params }
+    context 'negative tests' do
+      include_examples 'invalid params follower_id or followed_id'
+      context 'the relationship already exists' do
+        let(:params) { { follower_id: follower.id, followed_id: followed.id } }
+        before do
+          follower.follow(followed)
+          execute
+        end
+        it 'should return error message' do
+          json = JSON.parse(response.body)
+          expect(json['error_message']).to eq('relationship already exists')
+        end
+        it_behaves_like '422'
+      end
+    end
+
+    context 'positive tests' do
+      let(:params) { { follower_id: follower.id, followed_id: followed.id } }
+      before { execute }
+      it 'the relationship should be created' do
+        relationship = Relationship.find_by(follower: follower, followed: followed)
+        expect(relationship).not_to be_nil
+      end
+      it_behaves_like '204'
+    end
+  end
+
+  # to unfollow an user
+  # required params: follower_id, followed_id
+  describe 'POST /api/unfollow' do
+    let(:execute) { post '/api/unfollow', params: params }
+    context 'negative tests' do
+      include_examples 'invalid params follower_id or followed_id'
+      context 'the relationship not found' do
+        let(:params) { { follower_id: follower.id, followed_id: followed.id } }
+        before { execute }
+        it 'should return error message' do
+          json = JSON.parse(response.body)
+          expect(json['error_message']).to eq('relationship not found')
+        end
+        it_behaves_like '422'
+      end
+    end
+
+    context 'positive tests' do
+      let(:params) { { follower_id: follower.id, followed_id: followed.id } }
+      before do
+        follower.follow(followed)
+        execute
+      end
+      it 'the relationship should be removed' do
+        relationship = Relationship.find_by(follower: follower, followed: followed)
+        expect(relationship).to be_nil
+      end
+      it_behaves_like '204'
+    end
+  end
+end
--- spec/support/shared.rb
+++ spec/support/shared.rb
@@ -6,6 +6,7 @@ def error_message(response)
   json['error_message']
 end
 
+# shared examples for clock_ins api
 shared_examples_for 'invalid params user_id' do
   context 'without user_id' do
     let(:params) { {} }
@@ -38,7 +39,72 @@ shared_examples_for 'invalid params user_id' do
   end
 end
 
-codes = [422, 404, 406, 200, 201, 202]
+# shared examples for relationship api
+shared_examples_for 'invalid params follower_id or followed_id' do
+  # invalid follower_id
+  context 'without follower_id' do
+    let(:params) { { followed_id: followed.id } }
+    before { execute }
+    it 'should return error message' do
+      expect(error_message response).to eq('Couldn\'t find User without an ID')
+    end
+
+    it_behaves_like '404'
+  end
+
+  context 'invalid follower_id' do
+    let(:invalid_id) { 'random string' }
+    let(:params) { { follower_id: invalid_id, followed_id: followed.id } }
+    before { execute }
+    it 'should return error message' do
+      expect(error_message response).to eq("Couldn't find User with 'id'=#{invalid_id}")
+    end
+    it_behaves_like '404'
+  end
+
+  context 'user not exist' do
+    let(:non_existing_user_id) { User.last&.id.to_i + 1 }
+    let(:params) { { follower_id: non_existing_user_id, followed_id: followed.id } }
+    before { execute }
+    it 'should return error message' do
+      expect(error_message response).to eq("Couldn't find User with 'id'=#{non_existing_user_id}")
+    end
+    it_behaves_like '404'
+  end
+
+  # invalid followed_id
+  context 'without followed_id' do
+    let(:params) { { follower_id: follower.id } }
+    before { execute }
+    it 'should return error message' do
+      expect(error_message response).to eq('Couldn\'t find User without an ID')
+    end
+
+    it_behaves_like '404'
+  end
+
+  context 'invalid followed_id' do
+    let(:invalid_id) { 'random string' }
+    let(:params) { { follower_id: invalid_id, followed_id: followed.id } }
+    before { execute }
+    it 'should return error message' do
+      expect(error_message response).to eq("Couldn't find User with 'id'=#{invalid_id}")
+    end
+    it_behaves_like '404'
+  end
+
+  context 'user not exist' do
+    let(:non_existing_user_id) { User.last&.id.to_i + 1 }
+    let(:params) { { followed_id: non_existing_user_id, follower_id: followed.id } }
+    before { execute }
+    it 'should return error message' do
+      expect(error_message response).to eq("Couldn't find User with 'id'=#{ non_existing_user_id }")
+    end
+    it_behaves_like '404'
+  end
+end
+
+codes = [422, 404, 406, 200, 201, 202, 204]
 codes.each do |code|
   shared_examples code.to_s do
     specify "return #{code}" do
