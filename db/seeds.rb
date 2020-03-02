b0f29bcba8cd862bc5491fcbb899f83f6432e771
MLHustMaroon <doxuan.thanh@monstar-lab.com>
2020-03-03 02:05:43 +0900

    Created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# users
+10.times do |i|
+  User.create(name: "user_#{i + 1}")
+  puts "Created user user_#{i + 1}"
+end
+
+User.all.each do |u|
+  10.times do
+    sleep_time = rand(4..10)
+    u.clock_ins.create(
+        sleep_time: Time.now - sleep_time.hours,
+        wakeup_time: Time.now,
+        clocked_in_time: sleep_time,
+        created_at: rand((Time.now - 10.days)..Time.now)
+    )
+  end
+  puts "created 10 clock_ins for #{u.name}"
+  random_user = rand(1..10)
+  user = User.find_by(id: random_user)
+  if user && u != user
+    u.follow(user)
+    puts "#{u.name} followed #{user.name}"
+  end
+end
+
